8d1f009cbfeb8dc8809c53843869140222fbdd6d
SoeunShin <61735948+SoeunShin@users.noreply.github.com>
2022-01-23 22:30:07 +0900

    Update A086_신소은_20220117.py

--- A086_신소은_20220117.py
+++ A086_신소은_20220117.py
@@ -2,7 +2,7 @@
 
 m, n = map(int, input().split())
 dic = {'0':'9', '1':'4', '2':'8', '3':'7', '4':'2', '5':'1', '6':'6', '7':'5', '8':'0', '9':'3'}
-lst = []
+lst = [] 
 
 for i in range(m, n+1): 
     itoa = ' '.join([dic[j] for j in str(i)]) 
