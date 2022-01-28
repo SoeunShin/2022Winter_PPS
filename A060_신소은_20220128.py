a3d2ef64fcd7a9f42ea495dab4a2222cdef969d3
SoeunShin <61735948+SoeunShin@users.noreply.github.com>
2022-01-28 17:18:52 +0900

    20220128

--- A060_신소은_20220128.py
+++ A060_신소은_20220128.py
@@ -9,4 +9,4 @@ class Solution:
                 if i == "C": arr.pop()
                 elif i == "D": arr.append(arr[-1]*2)
                 elif i == "+": arr.append(arr[-1]+arr[-2])
-        return sum(arr)
+        return sum(arr) 
